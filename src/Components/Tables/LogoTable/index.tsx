66d9c6facd8f7fd9d16c1bcd3d4d4dd6324c7367
konika098 <konikakhan2000@gmail.com>
2025-02-11 12:57:00 +0600

    ADD

--- src/Components/Tables/LogoTable/index.tsx
+++ src/Components/Tables/LogoTable/index.tsx
@@ -31,7 +31,9 @@ const LogoTable: React.FC<TableProps> = (props) => {
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-full">
         <table className="w-full h-full" style={{ minWidth: "400px" }}>
           <thead
-            className={`text-xs uppercase ${props.themeMode ? " text-gray-700  bg-gray-400" : "bg-gray-700 text-gray-400"}`}
+            className={`text-xs uppercase ${props.themeMode
+              ? "text-white bg-[#5A1073]"
+              : "bg-[#3bd6c6] text-[#5A1073]"}`}
           >
             <tr>
               <th className="px-6 py-3" style={{ width: "200px" }}>
