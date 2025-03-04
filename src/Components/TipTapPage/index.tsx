ba6521aefef58589d1eda269aa9b9079036fdfee
arinahuque23 <arinahuqe23@gmail.com>
2025-03-04 16:25:05 +0600

    updated

--- src/Components/TipTapPage/index.tsx
+++ src/Components/TipTapPage/index.tsx
@@ -135,7 +135,7 @@ const NewsPreviewModal = ({ isOpen, onClose, article, dispatch }: any) => {
       size="lg" // Smaller modal size
       motionPreset="scale"
     >
-      {/* <YoutubePlayer isOpen={isOpen} /> */}
+      <YoutubePlayer isOpen={isOpen} />
       <ModalOverlay backdropFilter="blur(10px)" />
       <ModalContent className="rounded-lg p-4 shadow-xl" style={{ maxWidth: '1300px', margin: '0 auto' }}>
         <ModalCloseButton
